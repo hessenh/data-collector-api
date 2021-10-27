2dc44b72b91bf5134dc1af1201f3c0f87e13e1de
fenglin-ssb <fnh@ssb.no>
2021-10-27 18:16:13 +0200

    bug fix

--- src/main/java/no/ssb/dc/api/el/Secret.java
+++ src/main/java/no/ssb/dc/api/el/Secret.java
@@ -19,7 +19,7 @@ public class Secret {
             Map<String, String> secretProviderMap = new LinkedHashMap<>();
             secretProviderMap.put("secrets.provider", secretProvider);
             secretProviderMap.put("secrets.project-id", projectId);
-            if (saPath != "ENGINE") {
+            if (!saPath.contains("ENGINE") ) {
                 secretProviderMap.put("secrets.service-account-key-path", saPath);
             }
             try {
