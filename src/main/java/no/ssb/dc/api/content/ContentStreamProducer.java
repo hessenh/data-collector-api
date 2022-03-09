44cb8e9f4383467327e3252b08964ab0c1e6f9c2
rupinder <Rupinder.Kaur@ssb.no>
2022-03-09 14:10:28 +0100

    Added support for RawdataMessage class

--- src/main/java/no/ssb/dc/api/content/ContentStreamProducer.java
+++ src/main/java/no/ssb/dc/api/content/ContentStreamProducer.java
@@ -1,28 +1,27 @@
 package no.ssb.dc.api.content;
 
-import java.util.Arrays;
+import no.ssb.rawdata.api.RawdataMessage;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 public interface ContentStreamProducer extends AutoCloseable {
 
     ContentStreamBuffer.Builder builder();
 
-    ContentStreamProducer copy(ContentStreamBuffer buffer);
+    RawdataMessage copy(ContentStreamBuffer buffer);
 
-    ContentStreamProducer produce(ContentStreamBuffer.Builder bufferBuilder);
+    RawdataMessage produce(ContentStreamBuffer.Builder bufferBuilder);
 
     default void publishBuilders(ContentStreamBuffer.Builder... builders) throws ClosedContentStreamException {
         for (ContentStreamBuffer.Builder builder : builders) {
-            produce(builder);
+            publish(produce(builder));
         }
-        publish(Arrays.stream(builders).map(ContentStreamBuffer.Builder::position).collect(Collectors.toList()));
     }
 
-    default void publish(List<String> positions) throws ClosedContentStreamException {
-        publish(positions.toArray(new String[positions.size()]));
+    default void publish(List<RawdataMessage> rawdataMessages) throws ClosedContentStreamException {
+        publish(rawdataMessages.toArray(new RawdataMessage[rawdataMessages.size()]));
     }
 
-    void publish(String... position);
+    void publish(RawdataMessage... rawdataMessages);
 
 }
